fa2546f259a0559126490bbf95da6bb71b94e588
Jessica Chu <jessicachu81@gmail.com>
2019-11-29 12:44:15 -0800

    updated

--- scripts/reminder.js
+++
@@ -1,8 +0,0 @@
-
-      //Retrieve course info from database and place onto HTML
-      db.collection("courses").doc("1536").onSnapshot(function (snap) {
-          console.log("Current data is...", snap.data());
-          document.getElementById('stuff').innerHTML = snap.data().name +" "+  snap.data().room;
-        
-
-      });
