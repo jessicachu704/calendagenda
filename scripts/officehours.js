72d102c216ce929986b3dcababd2d331bbcb03b3
Jessica Chu <jessicachu81@gmail.com>
2019-12-05 10:30:55 -0800

    updated

--- scripts/officehours.js
+++ scripts/officehours.js
@@ -50,3 +50,14 @@ info1113();
 info1930();
 info1712();
 info1510();
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
